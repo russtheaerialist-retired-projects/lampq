43066c67eb2ea5b581a0b3bf7da3f2484755d85b
Russell Hay <me@russellhay.com>
2014-11-03 19:57:16 -0800

    Trying to get the rotary encoder to work properly.  This approach might not be fast enough

--- lampy/lampy.ino
+++ lampy/lampy.ino
@@ -2,9 +2,13 @@
 
 #define ENCODER_PIN_1 2
 #define ENCODER_PIN_2 3
+#define DEBOUNCE_TIME 250
 #define MAX_MODES 10
 uint8_t old_AB = 0;
+int tmpdata;
 int mode=0, next_mode=0;
+uint8_t pin1, pin2;
+unsigned long last_read = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -12,13 +16,23 @@ void setup() {
   digitalWrite(ENCODER_PIN_1, HIGH); // Turn on Pullup
   pinMode(ENCODER_PIN_2, INPUT);
   digitalWrite(ENCODER_PIN_2, HIGH); // Turn on Pullup
-
+  Serial.println("start");
 }
 
 void loop() {
-  if (mode != next_mode) {
-    mode = next_mode;
-    Serial.println(mode);
+  if (millis() - last_read > DEBOUNCE_TIME) {
+    tmpdata = read_encoder();
+
+    if (tmpdata) {
+      last_read = millis();
+      next_mode += tmpdata;
+      if (next_mode < 0) {
+        next_mode = MAX_MODES - 1;
+      } else {
+        mode = next_mode % MAX_MODES;
+      }
+      Serial.println(mode);
+    }
   }
 }
 
@@ -27,7 +41,8 @@ int8_t read_encoder()
   static int8_t enc_states[] = {0,-1,1,0,1,0,0,-1,-1,0,0,1,0,1,-1,0}; 
 
   old_AB <<= 2;                   //remember previous state
-  old_AB |= ((digitalRead(ENCODER_PIN_2)<<1) | digitalRead(ENCODER_PIN_1));
-  next_mode += ( enc_states[( old_AB & 0x0f )]);
-  next_mode %= MAX_MODES;
+  pin1 = digitalRead(ENCODER_PIN_2);
+  pin2 = digitalRead(ENCODER_PIN_1);
+  old_AB |= (pin2<<1) | pin1;
+  return ( enc_states[( old_AB & 0x0f )]);
 }
