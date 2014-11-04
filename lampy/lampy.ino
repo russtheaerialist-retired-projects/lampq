2a270c2e9a61a33ebcd70b1ee6d5aa8f6e32bc7d
Russell Hay <me@russellhay.com>
2014-11-03 20:57:02 -0800

    Encoder is working

--- lampy/lampy.ino
+++ lampy/lampy.ino
@@ -4,7 +4,8 @@
 #define ENCODER_PIN_1 2
 #define ENCODER_PIN_2 3
 #define MAX_MODES 10
-int mode=0, next_mode=0;
+int mode=0, next_mode=0, data=0;
+int old_position=0;
 Encoder control(ENCODER_PIN_1, ENCODER_PIN_2);
 
 void setup() {
@@ -13,7 +14,12 @@ void setup() {
 }
 
 void loop() {
-  next_mode += control.read();
+  data = control.read();
+  if ((data != old_position)&&(data%4 == 0)) {
+    next_mode += (data - old_position)/4;
+    old_position = data;
+  }
+  
   if(next_mode != mode) {
     if (next_mode < 0) {
       next_mode = MAX_MODES - 1;
