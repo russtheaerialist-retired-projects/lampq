00c02404abd1b36501c36a7780f6205358d7cbb3
Russell Hay <me@russellhay.com>
2014-11-05 06:58:04 -0800

    Adding color palette changing mode

--- lampy/lampy.ino
+++ lampy/lampy.ino
@@ -8,7 +8,7 @@
 
 #define ENCODER_PIN_1 7
 #define ENCODER_PIN_2 8
-#define MAX_MODES 6
+#define MAX_MODES 7
 #define FRAME_DELAY 500
 
 int mode=0, next_mode=0, data=0;
@@ -17,25 +17,27 @@ Encoder control(ENCODER_PIN_1, ENCODER_PIN_2);
 CRGB leds[NUM_LEDS];
 long last_frame = 0;
 
-mode_entry_t modes[MAX_MODES] = {
+const mode_entry_t modes[MAX_MODES] = {
   {-1, off},
   {-1, all_blue},
   {-1, all_red},
   {-1, all_green},
   {500, police},
-  {100, random_decay}
+  {100, random_decay},
+  {100, palette_swap}
 };
 
 void setup() {
   Serial.begin(9600);
-  FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
   FastLED.clear();
   FastLED.show();
 }
 
 void loop() {
   data = control.read();
-  Serial.println(data);
+  Serial.print(data);
+  Serial.print("-");
   if ((data != old_position)&&(data%4 == 0)) {
     next_mode += (data - old_position)/4;
     old_position = data;
@@ -48,10 +50,12 @@ void loop() {
     next_mode %= MAX_MODES;
     mode = next_mode;
     last_frame = 0;
+    FastLED.clear();
   }
   
   if (last_frame == 0 || (millis() - last_frame > modes[mode].delay && modes[mode].delay > 0)) {
     modes[mode].func(leds, NUM_LEDS);
     last_frame = millis();
   }
+  Serial.println(mode);
 }
--- lampy/modes.h
+++ lampy/modes.h
@@ -9,24 +9,42 @@ typedef struct {
   voidfunc_t func;
 } mode_entry_t;
 int step=0;
+uint8_t startIndex = 0;
+CRGBPalette16 currentPalette = RainbowColors_p;
+TBlendType    currentBlend = BLEND;
 
-void all_blue(CRGB* leds, uint8_t numPix) {
-  FastLED.showColor(CRGB::Blue);
-}
-
+////////////////////////////////////////////////////////////////////
+// Mode off - by Russell Hay
+// Turns off all of the leds
 void off(CRGB* leds, uint8_t numPix) {
   FastLED.clear();
   FastLED.show();
 }
 
+////////////////////////////////////////////////////////////////////
+// Mode Blue - by Russell Hay
+// Turns all of the leds Blue
+void all_blue(CRGB* leds, uint8_t numPix) {
+  FastLED.showColor(CRGB::Blue);
+}
+
+////////////////////////////////////////////////////////////////////
+// Mode Red - by Russell Hay
+// Turns all of the leds Red
 void all_red(CRGB* leds, uint8_t numPix) {
   FastLED.showColor(CRGB::Red);
 }
 
+////////////////////////////////////////////////////////////////////
+// Mode Green - by Russell Hay
+// Turns all of the leds Green
 void all_green(CRGB* leds, uint8_t numPix) {
   FastLED.showColor(CRGB::Green);
 }
 
+////////////////////////////////////////////////////////////////////
+// Mode Police - by Russell Hay
+// Alternating Red and Blue leds that switch color every half second
 void police(CRGB* leds, uint8_t numPix) {
   for(int i=0;i<numPix;i++) {
     leds[i] = (i+step) % 2 ? CRGB::Blue : CRGB::Red;
@@ -35,6 +53,10 @@ void police(CRGB* leds, uint8_t numPix) {
   FastLED.show();
 }
 
+////////////////////////////////////////////////////////////////////
+// Mode RandomDecay - by Russell Hay
+// 25% change of a random led turning on, and then fading out to
+// it's neighbors
 void random_decay(CRGB *leds, uint8_t numPix) {
   int r, g, b;
   for(int i=0;i<numPix;i++) {
@@ -61,9 +83,56 @@ void random_decay(CRGB *leds, uint8_t numPix) {
     leds[i].b = max(qadd8(b, 0) - 5, 0);
   }
   
-  if (random8(100) < 10) { 
+  if (random8(100) < 25) { 
     leds[random(numPix)] = CRGB(random8(), random8(), random8());
   }
+  FastLED.show();
+}
+
+
+////////////////////////////////////////////////////////////////////
+// Mode Periodic Color Palette Change - by FastLED
+// This mode taken from the FastLED example
+// https://github.com/FastLED/FastLED/blob/master/examples/ColorPalette/ColorPalette.ino
+void _ChangePalettePeriodically() {
+  uint8_t secondHand = (millis() / 1000) % 60;
+  
+  if (step != secondHand) {
+    step = secondHand;
+    switch(secondHand) {
+      case 0:
+        currentPalette = RainbowColors_p;
+        currentBlend = BLEND;
+        break;
+      case 15:
+        currentPalette = PartyColors_p;
+        currentBlend = BLEND;
+        break;
+      case 30:
+        currentPalette = RainbowStripeColors_p;
+        currentBlend = NOBLEND;
+        break;
+      case 45:
+        currentPalette = CloudColors_p;
+        currentBlend = BLEND;
+        break;
+    }
+  }
+}
+
+void _FillLEDsFromPaletteColors(CRGB* leds, uint8_t numPix, uint8_t colorIndex) {
+  for(int i=0; i<numPix; i++) {
+    leds[i] = ColorFromPalette(currentPalette, colorIndex, 255 /* Brightness */, currentBlend);
+    colorIndex += 3;
+  }
+}
+
+void palette_swap(CRGB* leds, uint8_t numPix) {
+  _ChangePalettePeriodically();
+ 
+  startIndex += 1;  // Motion Speed;
+  
+  _FillLEDsFromPaletteColors(leds, numPix, startIndex);
   FastLED.show();
 }
 
